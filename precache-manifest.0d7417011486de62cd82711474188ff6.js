c96b5aaf74acc683e968a9b6545773b199b11207
anuj sharma <anujjangra25119@gmail.com>
2019-08-26 22:45:00 +0530

    Updates

---
+++ precache-manifest.0d7417011486de62cd82711474188ff6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "352823f6b030415331cc",
+    "url": "nitkosg.github.io/static/css/main.42a53202.chunk.css"
+  },
+  {
+    "revision": "352823f6b030415331cc",
+    "url": "nitkosg.github.io/static/js/main.352823f6.chunk.js"
+  },
+  {
+    "revision": "4c333738a3baa30e5a89",
+    "url": "nitkosg.github.io/static/css/1.08564658.chunk.css"
+  },
+  {
+    "revision": "4c333738a3baa30e5a89",
+    "url": "nitkosg.github.io/static/js/1.4c333738.chunk.js"
+  },
+  {
+    "revision": "4843cdf644e9a087e74d",
+    "url": "nitkosg.github.io/static/js/runtime~main.4843cdf6.js"
+  },
+  {
+    "revision": "b6e6ddbde5aa10a0eac3ed45cc3a5dd2",
+    "url": "nitkosg.github.io/static/media/pic1.b6e6ddbd.png"
+  },
+  {
+    "revision": "53368b299c2437eab70336e469f52b17",
+    "url": "nitkosg.github.io/static/media/pic2.53368b29.png"
+  },
+  {
+    "revision": "8650311e51c0b2aa170773c8d0719f2b",
+    "url": "nitkosg.github.io/static/media/pic3.8650311e.png"
+  },
+  {
+    "revision": "d5ad159b83b28f23b4e6d35a1c25b0d0",
+    "url": "nitkosg.github.io/static/media/pic4.d5ad159b.png"
+  },
+  {
+    "revision": "9b3aa1b191da3eddb388836ddd8b8b0f",
+    "url": "nitkosg.github.io/static/media/SofiaProSemiBold.9b3aa1b1.woff"
+  },
+  {
+    "revision": "fe5aeef0b75ebada8ef9fb9fe7c07c37",
+    "url": "nitkosg.github.io/static/media/hero.fe5aeef0.jpg"
+  },
+  {
+    "revision": "4be1e1e4c17871ebdd3979b8893fe937",
+    "url": "nitkosg.github.io/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/NITKOSG_Website/precache-manifest.69f2dcbe674addb0c58cbe37d866b8ff.js"
+  "nitkosg.github.io/precache-manifest.0d7417011486de62cd82711474188ff6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/NITKOSG_Website/index.html", {
+workbox.routing.registerNavigationRoute("nitkosg.github.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
